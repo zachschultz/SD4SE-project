3a04b7c213b8784eae99c2d975227fdb41b2aea3
zachschultz <zacharymschultz@gmail.com>
2015-04-12 01:00:54 +0200

    removed API key

--- assignment-3/js/app.js
+++ assignment-3/js/app.js
@@ -2,9 +2,6 @@ $(function(){
 
   app = app || {};
 
-  gAPIKey = 'AIzaSyAMl9VXe8AasNNM32-wcPs0ZXkgWJFxcek';
-  nearbyReq = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='+gAPIKey+'&';
-
   // Run initialization code to get user's location
   app.initialize();
 
