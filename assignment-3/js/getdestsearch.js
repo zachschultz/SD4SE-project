d8281844188c85ddbb5a9744a82c9beacc23fc32
zachschultz <zacharymschultz@gmail.com>
2015-04-13 18:17:27 +0200

    More refactoring...a framework would definitely be nice here

--- assignment-3/js/getdestsearch.js
+++ assignment-3/js/getdestsearch.js
@@ -7,35 +7,34 @@ app.getDestSearch = function(dest_str) {
   geocoder = new google.maps.Geocoder();
 
   // Callback function for each result list item
-  function resultsCallback(i) {
+  function addToMapAndGetDirections() {
     return function() {
+
+      // Check if we already have a second marker on map, if so, remove it
       if (markers.length == 2) {
         var userMarker = markers[0];
-        // remove second marker
+        console.log("already have 2 markers");
+        // Erase all markers from map
         for (var i = 0; i < markers.length; i++) {
           console.log('setting '+markers[i].title+' map to null');
           markers[i].setMap(null);
           markers[i] = null;
         }
-        markers = []
+        // Reset markers array, push original marker
+        markers = [];
         markers[0] = userMarker;
         markers[0].setMap(map);
 
-
-        // remove directions
-        console.log(typeof directionsDisplay);
         // If we have directions already, remove it from the map
-        // also, remove the existing directions panel div
-        if (typeof directionsDisplay !== 'undefined') {
-          console.log("we have directions already");
-          directionsDisplay.setMap(null);
-          directionsDisplay = null;
-          $('#directions-panel').empty();
-        }
-
+        //  also, refresh the existing directions panel div
+        // if (typeof directionsDisplay !== 'undefined') {
+        //   directionsDisplay.setMap(null);
+        //   directionsDisplay = null;
+        //   $('#directions-panel').empty();
+        // }
       }
+
       app.loadDestOnMap($(this));
-      console.log(i);
     };
   }
 
@@ -59,7 +58,7 @@ app.getDestSearch = function(dest_str) {
           var $res = $('<li><div class="panel panel-default"><div class="panel-heading"><h3 id="result" data-id="'+i+'" data-lat="'+lat+'" data-long="'+long+'" class="panel-title">'+addr+'</h3></div></div></li>');
 
           $ul.append($res);
-          $res.on('click', resultsCallback(i));
+          $res.on('click', addToMapAndGetDirections());
 
           // Lighten up result panel on user mouseover
           $res.find('div.panel-heading').on('mouseover', function() {
--- assignment-3/js/loaddestonmap.js
+++ assignment-3/js/loaddestonmap.js
@@ -2,52 +2,60 @@ var app = app || {};
 
 app.loadDestOnMap = function($this) {
 
-  directionsDisplay = undefined;
+  // Google Maps API variables
   var directionsService = new google.maps.DirectionsService();
 
   var $panelTitle = $this.find('h3#result')[0];
   var dest_lat = $panelTitle.dataset.lat;
   var dest_lng = $panelTitle.dataset.long;
-  console.log();
 
-  // get lat/long of dest, make a LatLng objject
+  // Make a LatLng object for Google Maps
   var destLatLng = new google.maps.LatLng(dest_lat, dest_lng);
-  // make marker, add it to map
+  // Create a dest. marker, add it to map
   var destMarker = new google.maps.Marker({
     position: destLatLng,
     map: map,
     title:$panelTitle.innerText,
   });
 
-
   markers.push(destMarker);
-  console.log('adding marker' +destMarker.title+' to markers array: ');
-  console.log('[0] is '+markers[0].title);
-  console.log('[1] is '+markers[1].title);
 
-  $travelMode = $('select#travel-mode');
-  $travelModeContainer.show();
-  console.log($travelMode);
-
-  calcRoute();
-
+  // Make an infowindow for the destination marker
   var dest_infowindow = new google.maps.InfoWindow({
     content: $panelTitle.innerText,
   });
-
   google.maps.event.addListener(destMarker, 'click', function() {
-
     dest_infowindow.open(destMarker.get('map'), destMarker);
     map.setCenter(destMarker.getPosition());
   });
 
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  directionsDisplay.setMap(map);
-  directionsDisplay.setPanel(document.getElementById('directions-panel'));
+  // Make a direction panel to give readable directions
+  // directionsDisplay = new google.maps.DirectionsRenderer();
+  // directionsDisplay.setMap(map);
+  // directionsDisplay.setPanel(document.getElementById('directions-panel'));
 
+
+
+  // Show travel mode options and get travel mode selector (initially `DRIVING`)
+  $travelModeContainer.show();
+  $travelMode = $('select#travel-mode');
+  // Add event listener to recalculate route based on travel mode
   $travelMode.on('change', calcRoute);
 
+  // Now that we have a destination with a marker, calculate the route
+  calcRoute();
+
   function calcRoute() {
+    if (typeof directionsDisplay !== 'undefined') {
+      console.log('we already have directions!');
+      directionsDisplay.setMap(null);
+      directionsDisplay = undefined;
+      $('#directions-panel').empty();
+    }
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
     var selectedMode = $travelMode[0].value;
     var start = markers[0].position;
     var end = markers[1].position;
@@ -60,7 +68,10 @@ app.loadDestOnMap = function($this) {
     directionsService.route(request, function(response, status) {
       if (status == google.maps.DirectionsStatus.OK) {
         directionsDisplay.setDirections(response);
+      } else {
+        alert('Sorry, I couldn\'t find any routes for that method of travel.');
       }
+
     });
   }
 };
